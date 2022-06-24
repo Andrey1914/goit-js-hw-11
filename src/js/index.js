aaa70271ec7d44358c9960b99024c8315d3e6e6a
Andrey1914 <anlikajw@gmail.com>
2022-06-24 23:25:54 +0200

    first commit added src folder

---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import fetchImages from './fetchImages';
+// import cardTemplate from '../js/template/cardTemplate.hbs';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import throttle from 'lodash.throttle';
